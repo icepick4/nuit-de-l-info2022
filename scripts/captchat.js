d1c7623e23edf496745d868426ba473b2ac6dbbb
qypol342 <qypol342@gmail.com>
2022-12-02 03:44:31 +0100

    add better alert for captchat

--- scripts/captchat.js
+++ scripts/captchat.js
@@ -1,3 +1,5 @@
+const capotes = document.getElementsByClassName('capote');
+
 let checkBox = document.querySelector('.captchat');
 checkBox.addEventListener('click', function (e) {
     captchat();
@@ -34,9 +36,9 @@ function captchat(argument) {
 
     for (var i = 0; i < 9; i++) {
         if (i !== g) {
-            let index = Math.floor(Math.random() * bb.length);
+            let index_g = Math.floor(Math.random() * bb.length);
 
-            let img_path = bb[index];
+            let img_path = bb[index_g];
             let img = document.createElement('img');
             img.src = 'assets/captchat/' + img_path;
             img.name = img_path;
@@ -44,14 +46,14 @@ function captchat(argument) {
                 toggle_img(ev.target);
             });
             img.tabIndex = 1;
-            bb[index] = bb[bb.length - 1];
+            bb[index_g] = bb[bb.length - 1];
             bb.pop();
 
             capt.appendChild(img);
         } else {
-            let index = Math.floor(Math.random() * gg.length);
+            let index_g = Math.floor(Math.random() * gg.length);
 
-            let img_path = gg[index];
+            let img_path = gg[index_g];
             let img = document.createElement('img');
             img.src = 'assets/captchat/' + img_path;
             img.name = img_path;
@@ -59,7 +61,7 @@ function captchat(argument) {
                 toggle_img(ev.target);
             });
             img.tabIndex = 1;
-            gg[index] = gg[gg.length - 1];
+            gg[index_g] = gg[gg.length - 1];
             gg.pop();
             capt.appendChild(img);
         }
@@ -72,10 +74,11 @@ function captchat(argument) {
     let btn_close = document.createElement('button');
     btn_close.classList.add('close');
     btn_close.innerHTML = 'X';
-    btn_close.addEventListener('click', close);
+    btn_close.addEventListener('click', close_cap);
 
     capt.appendChild(btn_close);
     document.body.appendChild(capt);
+    toggleModal();
 }
 
 // select/ deselect img
@@ -106,7 +109,7 @@ function validate(ev) {
 }
 
 // close captchat
-function close(ev) {
+function close_cap(ev) {
     //checkBox.style.backgroundColor="red";
     checkBox.querySelector('#check_capt').classList.remove('check_valide');
     document.body.removeChild(document.querySelector('.captchat_select'));
